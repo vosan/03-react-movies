c16895158ff04de9303c375d425d3861456a439f
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-09-08 18:01:00 -0700

    feat: integrate movie search experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "modern-normalize/modern-normalize.css";
+import "./index.css";
+import App from "./components/App/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
